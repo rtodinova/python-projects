578a78709e8ae7e34353d09c6d926c6f612d8076
rtodinova <rtodinova@gmail.com>
2019-02-26 13:55:31 +0200

    Interactive dictionary project included

---
+++ Interactive Dictionary/interactive-dictionary.py
@@ -0,0 +1,35 @@
+import json
+from difflib import get_close_matches
+
+words = json.load(open("data.json"))
+
+def display_definition(word):
+    word = word.lower()
+    if word in words:
+        return words[word]
+    elif word.title() in words:
+        return words[word.title()]
+    elif word.upper() in words:
+        return words[word.upper()]
+    elif get_close_matches(word, words.keys(), cutoff=0.8):
+        similar_word = get_close_matches(word, words.keys(), cutoff=0.8)[0]
+        answer = input("Did you mean %s instead? (y/n): " % similar_word)
+        if answer.lower() == 'y':
+            return words[similar_word]
+        else:
+            return "The word '%s' doesn't exist. Please double check it." % word
+    else:
+        return "The word '%s' doesn't exist. Please double check it." % word
+
+def run():
+    word = input("Enter a word: ")
+    output = display_definition(word)
+    if type(output) == list:
+        print("'%s' definitions:" % word)
+        for definition in output:
+            print(definition)
+    else:
+        print(output)
+
+if __name__ == '__main__':
+    run()
